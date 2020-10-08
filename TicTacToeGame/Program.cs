6d563c31facb3496fbcb53339262b46462cfc07c
Vishal Garg <vishugarg10004@gmail.com>
2020-10-08 10:09:44 +0530

    Add project files.

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    class Program
+    {
+        public static char[] board = new char[10];
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocome to TicTacToe Game");
+            CreatingBoard();
+            TicTacToe ticTacToe = new TicTacToe();
+            TicTacToe.Board(board);
+        }
+        public static char[] CreatingBoard()
+        {
+            for (int k = 0; k < board.Length; k++)
+            {
+                board[k] = ' ' ;
+            }
+            return board;
+
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    class TicTacToe
+    {
+        
+        
+        public static void Board(char[] boardPositions)
+        {
+            Console.WriteLine("Below is the board of TicTacToe"); 
+            Console.WriteLine($"{boardPositions[1]} | {boardPositions[2]} |{ boardPositions[3]}");
+            Console.WriteLine("__|___|__");
+            Console.WriteLine($"{boardPositions[4]} | {boardPositions[5]} | { boardPositions[6]}");
+            Console.WriteLine("__|___|__");
+            Console.WriteLine($"{boardPositions[7]} | {boardPositions[8]} | { boardPositions[9]}");
+        }
+
+    }
+}
