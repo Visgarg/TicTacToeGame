e52de8aa3a6afc9efeb67f412855cb9f70b55ce6
Vishal Garg <vishugarg10004@gmail.com>
2020-10-08 11:16:37 +0530

    [Vishal] added code for displaying board.

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -12,24 +12,25 @@ namespace TicTacToeGame
         {
             char[] charactersArray = new char[2];
             Console.WriteLine("Welocome to TicTacToe Game");
+            //Array created for board positions
             CreatingBoard();
-            //TicTacToe.Board(board);
+            //choosing character for player
             charactersArray= TicTacToe.ChoosingCharacter();
             Console.WriteLine($"you will play with :{charactersArray[0]}");
             Console.WriteLine($"CPU will play with :{charactersArray[1]}");
+            //displaying the board.
+            TicTacToe.Board(board);
         }
         /// <summary>
         /// Creatings the array of positions to be entered into array.
         /// </summary>
         /// <returns> empty positions for board in array</returns>
-        public static char[] CreatingBoard()
+        public static void CreatingBoard()
         {
             for (int k = 0; k < board.Length; k++)
             {
                 board[k] = ' ' ;
             }
-            return board;
-
         }
     }
 }
